2116d2138890114e378b8cb7d3a3c0901bb13fbd
mostafa.aitbaallal@gmail.com <mostafa.aitbaallal@gmail.com>
2022-07-31 20:11:42 +0000

    Add TreeStudent DTO

---
+++ Core/Domain/DTOs/CreateTreeStudentRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeKnowledgeSystem.Domain.DTOs
+{
+    public class CreateTreeStudentRequest
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string StudentName { get; set; }
+
+        [Required]
+        public DateTime? DateOfBirth { get; set; }
+
+    }
+}
